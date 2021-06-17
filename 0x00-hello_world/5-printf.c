b03cfbe838f18f0480ea9a43c3dceb2a38cd4e00
=binikiya <=>
2021-06-17 05:30:05 -0700

    My first C prog commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-int main(void){
-  printf("with proper grammer, but the outcome is a piece of art,\n");
 
-  return (0);
+/**
+ *main - Prints  "with proper grammer, but the outcome is a piece of art,"
+ *followed by a new line
+ *
+ *Return: Always 0
+ */
+
+int main(void){
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
 }
