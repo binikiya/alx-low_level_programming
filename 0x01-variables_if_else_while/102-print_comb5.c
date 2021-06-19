e16755adf4dfa33d41b7062a66836ea7803c7c24
=binikiya <=>
2021-06-19 04:06:39 -0700

    Variables if else while commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int n1, n2;
 
 for (n1 = 0; n1 <= 98; n1++)
 {
-for (n2 = n1 + 1; n2 <= 98; n2++)
+for (n2 = n1 + 1; n2 <= 99; n2++)
 {
 putchar((n1 / 10) + '0');
 putchar((n1 % 10) + '0');
@@ -22,7 +22,7 @@ putchar((n2 % 10) + '0');
 
 if (n1 == 98 && n2 == 99)
 continue;
- 
+
 putchar(',');
 putchar(' ');
 }
