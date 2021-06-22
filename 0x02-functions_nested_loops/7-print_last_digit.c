8715009056ae7cf20fd8bb06263f0b9ed18645dc
Biniyam Tamene <binitame123@gmail.com>
2021-06-22 06:57:29 -0700

    Functions and nested loops commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,11 @@
 
 int print_last_digit(int n)
 {
-int lastDigit;
-if (n < 0)
-n *= -1;
-lastDigit = n % 10;
+int lastDigit n % 10;
+if (lastDigit < 0)
+lastDigit *= -1;
+ 
 _putchar(lastDigit + '0'); 
+
 return (lastDigit);
 }
