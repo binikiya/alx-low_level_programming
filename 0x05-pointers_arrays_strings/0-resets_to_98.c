fdb972061383b38659975c5ffe70ab5180b01d8f
Biniyam Tamene <binitame123@gmail.com>
2021-06-28 00:20:21 -0700

    Pointers Arrays String Commit

---
+++ 0x05-pointers_arrays_strings/0-resets_to_98.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+/**
+ * reset_to_98 - takes integer pointer and updates the value to 98
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory has poiters, arrays and strings
