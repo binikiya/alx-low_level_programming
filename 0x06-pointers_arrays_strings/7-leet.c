b4fb92caa8f9c6bb5318e231e1c6c211ffcb9453
Biniyam Tamene <binitame123@gmail.com>
2021-07-01 06:45:06 -0700

    More Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,19 @@
 
 char *leet(char *s)
 {
-char src[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-int encode[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-int i = 0;
-int j = 0;
-while (s[i] != '\0')
-{
-while (j < 10)
-{
-if (s[i] == src[j])
-{
-s[i] = encode[j];
-}
-j++;
-}
-i++;
-}
-return (s);
+  char c[] = "AaEeOoTtLl";
+  char n[] = "4433007711";
+  int i, j;
+
+  for (i = 0; s[i] != '\0'; i++)
+    {
+      for (j = 0; j < 10; j++)
+	{
+	  if (s[i] == c[j])
+	    {
+	      s[i] = n[j];
+	    }
+	}
+    }
+  return (s);
 }
