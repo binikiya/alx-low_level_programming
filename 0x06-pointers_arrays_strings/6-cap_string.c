1410b85b06879f57aba69e25cf659fcdc52586af
Biniyam Tamene <binitame123@gmail.com>
2021-07-01 06:26:36 -0700

    More Pointers Arrays Strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,36 @@
 #include<stdio.h>
 
 /**
- * *cap_string - capitalizes all words of a string
- * @s: string to be capitalized
- * Return: capitalized string
+ * *cap_string - capitalizes all words
+ * @s: char pointer
+ * Return: int
  */
 
 char *cap_string(char *s)
 {
-int i, j;
-i = 0;
-char sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-while (s[i] != '\0'){
-i++;
-}
-for (j = 0; j < i; j++)
-{
-  if (s[j] == 32 || s[j] == 44 || s[j] == 9 ||
-      s[j] == 10 || s[j] == 59 || s[j] == 34 ||
-      s[j] == 41 || s[j] == 125 || s[j] == 46 ||
-      s[j] == 123 || s[j] == 33 || s[j] == 63)
+  int i = 0;
+  int j;
+  while (s[i] != '\0')
     {
-      if (s[j + 1] >= 97 && s[j + 1] <= 122)
+      i++;
+    }
+  for (j = 0; j < i; j++)
+    {
+      if (s[j] == 32 || s[j] == 44 || s[j] == 59 ||
+	  s[j] == 33 || s[j] == 46 || s[j] == 63 ||
+	  s[j] == 34 || s[j] == 40 || s[j] == 41 ||
+	  s[j] == 125 || s[j] == 123 || s[j] == 9 ||
+	  s[j] == 10)
 	{
-	  s[j + 1] -= 32;
+	  if (s[j + 1] >= 97 && s[j + 1] <= 122)
+	    {
+	      s[j + 1] = s[j + 1] - 32;
+	    }
 	}
     }
-}
-if (s[0] >= 97 && s[0] <= 122)
-{
-s[0] -= 32;
-}
-return (s);
+  if (s[0] >= 97 && s[0] <= 122)
+    {
+      s[0] = s[0] - 32;
+    }
+  return (s);
 }
