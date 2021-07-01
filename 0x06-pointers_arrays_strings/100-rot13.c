308f1ed26dc6d32c3943aa304432c632036f64e5
Biniyam Tamene <binitame123@gmail.com>
2021-07-01 08:15:14 -0700

    More Pointers Arrays Strings Commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,16 +12,16 @@ char abc[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 char rot[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 int i;
 int j;
- for (i = 0; s[i] != '\0'; i++)
-   {
-     for (j = 0; j < 52; j++)
-       {
-	 if (s[i] == abc[j])
-	   {
-	     s[i] = rot[j];
-	     break;
-	   }
-       }
-   }
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == abc[j])
+{
+s[i] = rot[j];
+break;
+}
+}
+}
 return (s);
 }
--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,16 +8,30 @@
 
 void print_number(int n)
 {
-  unsigned int i;
+  char l, d;
+  int r;
   if (n < 0)
     {
       _putchar('-');
-      i *= -1;
+      l = ('0' - (n % 10));
+      n /= 10;
     }
-  i = n;
-  if (i / 10)
+  else
     {
-      print_number(i / 10);
+      l = ((n % 10) + '0');
+      n /= 10;
     }
-  _putchar(i % 10 + '0');
+  r = 0;
+  while (n > 0)
+    {
+      r = r * 10 + (n % 10);
+      n /= 10;
+    }
+  while (r > 0)
+    {
+      d = ((r % 10) + '0');
+      _putchar(d);
+      d /= 10;
+    }
+  _putchar(l);
 }
