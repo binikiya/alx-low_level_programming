8a55a20499a081232f4a04cacd11bdd6a8e8689a
Biniyam Tamene <binitame123@gmail.com>
2021-07-01 08:34:30 -0700

    More Pointers Arrays Strings Commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,30 +8,30 @@
 
 void print_number(int n)
 {
-  char l, d;
-  int r;
-  if (n < 0)
-    {
-      _putchar('-');
-      l = ('0' - (n % 10));
-      n /= -10;
-    }
-  else
-    {
-      l = ((n % 10) + '0');
-      n /= 10;
-    }
-  r = 0;
-  while (n > 0)
-    {
-      r = r * 10 + (n % 10);
-      n /= 10;
-    }
-  while (r > 0)
-    {
-      d = ((r % 10) + '0');
-      _putchar(d);
-      r /= 10;
-    }
-  _putchar(l);
+char l, d;
+int r;
+if (n < 0)
+{
+_putchar('-');
+l = ('0' - (n % 10));
+n /= -10;
+}
+else
+{
+l = ((n % 10) + '0');
+n /= 10;
+}
+r = 0;
+while (n > 0)
+{
+r = r * 10 + (n % 10);
+n /= 10;
+}
+while (r > 0)
+{
+d = ((r % 10) + '0');
+_putchar(d);
+r /= 10;
+}
+_putchar(l);
 }
