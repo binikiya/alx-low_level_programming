cb2995111cb250d4b047df3dfdf520be995c3bfa
Biniyam Tamene <binitame123@gmail.com>
2021-07-15 23:36:52 -0700

    More Molloc Free Commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *strnew = NULL;
-unsigned int i, j, n1, n2, count, par;
+unsigned int i, j, n1, n2;
 n1 = 0;
 n2 = 0;
-count = 0;
-par = n;
+int sign = n;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
@@ -26,19 +25,20 @@ while (s1[n1] != '\0')
 n1++;
 while (s2[n2] != '\0')
 n2++;
-if (n >= n2)
+if (sign >= n2)
 {
-par = n2;
+sign = n2;
 strnew = malloc(sizeof(char) * (n1 + n2 + 1));
 }
 else
-{
 strnew = malloc(sizeof(char) * (n1 + n + 1));
 if (strnew == NULL)
 return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
+for (i = 0; i < n1; i++)
+{
 strnew[i] = s1[i];
-for (j = 0; j < par; j++)
+}
+for (j = 0; j < sign; j++)
 {
 strnew[i++] = s2[j];
 }
