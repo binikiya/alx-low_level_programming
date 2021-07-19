6d5ce0457d75ae8f55b048b20dd0759bd6d4365a
Biniyam Tamene <binitame123@gmail.com>
2021-07-19 03:25:05 -0700

    Structures Typedef Commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * dog - structures
+ * struct dog - structures
  * @age: age
  * @name: name
  * @owner: owner
@@ -15,7 +15,7 @@ float age;
 char *owner;
 };
 
-typedef dog dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
