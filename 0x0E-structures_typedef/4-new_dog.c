16bdbce79422f233ddb8c3d5c07db631885b0cce
Biniyam Tamene <binitame123@gmail.com>
2021-07-19 03:30:55 -0700

    Structures Typedef Commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,26 +10,26 @@
  * Return: ptr
  */
 
-dog_t new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *ptr;
-  ptr = malloc(sizeof(dog_t));
-  if (ptr == NULL)
-    return (NULL);
-  if (name == NULL)
-    {
-      free(ptr);
-      free(owner);
-      return (NULL);
-    }
-  if (owner == NULL)
-    {
-      free(ptr);
-      free(name);
-      return (NULL);
-    }
-  ptr->name = name;
-  ptr->age = age;
-  ptr->owner = owner;
-  return (ptr);
+dog_t *ptr;
+ptr = malloc(sizeof(dog_t));
+if (ptr == NULL)
+return (NULL);
+if (name == NULL)
+{
+free(ptr);
+free(owner);
+return (NULL);
+}
+if (owner == NULL)
+{
+free(ptr);
+free(name);
+return (NULL);
+}
+ptr->name = name;
+ptr->age = age;
+ptr->owner = owner;
+return (ptr);
 }
