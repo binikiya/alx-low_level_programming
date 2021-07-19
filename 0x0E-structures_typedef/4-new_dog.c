212b74a33914cc152813d874ac1449873184a45d
Biniyam Tamene <binitame123@gmail.com>
2021-07-19 04:13:09 -0700

    Structures Typedef Commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *ptr;
-ptr = malloc(sizeof(dog_t));
+ptr = malloc(sizeof(struct dog));
 if (ptr == NULL)
 return (NULL);
 if (name == NULL)
