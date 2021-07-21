91bf6b46666c8b3e46289cf1314b7847a83fcd80
Biniyam Tamene <binitame123@gmail.com>
2021-07-21 03:48:34 -0700

    Function Pointers Commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: Sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub -substract two integers
+ * @a: first int
+ * @b: second int
+ * Return: substract
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integer
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - divedes two integers
+ * @a: first int
+ * @b: second int
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - modulus of integer
+ * @a: first int
+ * @b: second int
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
