4b4da07b0ebe73c791e8a22772f8cbed1e2f85e9
Biniyam Tamene <binitame123@gmail.com>
2021-08-05 09:00:28 -0700

    Bit Manipulation Commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 1 or 0
+ */
+int get_endiannes(void)
+{
+unsigned int i = 1;
+char *c = (char *)&i;
+if (*c)
+return (1);
+else
+return (0);
+}
