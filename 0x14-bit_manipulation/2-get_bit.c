75b6b243ff61ab43e1c3e742ceb65af11d0bb853
Biniyam Tamene <binitame123@gmail.com>
2021-08-05 06:06:20 -0700

    Bit Manipulation Commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,14 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-int bin = 0, var = 0;
 if (index > 63)
 return (-1);
-bin = (n >> index);
-var = bin & 1;
-if (var)
-return (1);
-else
-return (0);
-return (-1);
+return (n >> index & 1);
 }
