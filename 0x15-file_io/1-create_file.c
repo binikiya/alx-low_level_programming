6df1d631ea1206680db0f2ef64a42b0140344b75
Biniyam Tamene <binitame123@gmail.com>
2021-08-09 03:24:33 -0700

    File Input Output Commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * Return: 1 or -1
  */
 
-int text_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 int dest, size, i = 0;
 char *buffer;
