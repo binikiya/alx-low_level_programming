4670827100337c3df47a3a267ece8d432d3ddfa9
binikiya <biniyamkiya3682@gmail.com>
2021-09-16 06:39:23 -0700

    Doubly Linked Lists

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a new node at the begginig of a dlistint_t list
+ * @head: pointer to the first element of the list
+ * @n: integer
+ * Return: the pointer to a new node at the beggining of a dlistint_t list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *newnode = NULL;
+if (head == NULL)
+return (NULL);
+newnode = malloc(sizeof(dlistint_t));
+if (newnode == NULL)
+return (NULL);
+newnode->n = n;
+if (*head == NULL)
+{
+newnode->next = *head;
+newnode->prev = NULL;
+*head = newnode;
+return (newnode);
+}
+newnode->n = n;
+(*head)->prev = newnode;
+newnode->next = *head;
+*head = newnode;
+return (newnode);
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - return the nth node of a dlistint_t list
+ * @head: the first node of the list
+ * @index: number of node we want to know
+ * Return: NULL
+ */
+dlistint_t  *get_dnodeint_at_index(dnodeint_t *head, unsigned int index)
+{
+unsigned int i = 0;
+while (head != NULL)
+{
+if (i == index)
+return (head);
+i++;
+head = head->next;
+}
+return (NULL);
+}
