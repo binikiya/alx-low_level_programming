542442e09be71ee1fc0682429328565ea66eeeed
binikiya <biniyamkiya3682@gmail.com>
2021-09-21 02:33:54 -0700

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/strncpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * *_strncpy - copies a string
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: limit
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+for (i = 0; (i < n && src[i] != '\0'); i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
+}
