31ef75b238e2ebeb46cdcd543cf6212df17a26f8
binikiya <binikiya123@gmail.com>
2021-11-29 01:07:23 -0800

    Update Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,15 @@
 
 def island_perimeter(grid):
     """returns the perimeter of island described in grid"""
-    r = len(grid)
+    row = len(grid)
     p = 0
-    for i in range(r):
+    for i in range(row):
         c = len(grid[i])
         for j in range(c):
             if grid[i][j] == 1:
                 l = 1 if j == 0 or grid[i][j - 1] == 0 else 0
                 t = 1 if i == 0 or grid[i - 1][j] == 0 else 0
                 r = 1 if j == c - 1 or grid[i][j + 1] == 0 else 0
-                b = 1 if i == r - 1 or grid[i + 1][j] == 0 else 0
+                b = 1 if i == row - 1 or grid[i + 1][j] == 0 else 0
                 p += (l + t + r + b)
     return p
