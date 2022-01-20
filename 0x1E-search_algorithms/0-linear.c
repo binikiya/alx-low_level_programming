49a0e2d2f94cefd52a1c4ee5b9a3d1638cef3045
binikiya <biniyamkiya3682@gamil.com>
2022-01-20 02:13:56 -0800

    Search Algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algorithms.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: poiter to the first element of the array to search in
+ * @size: the number of elements in array
+ * @value: the value to search for
+ * Return: the first index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+size_t i;
+int tmp;
+if (array != NULL)
+{
+for (i = 0; i < size; i++)
+{
+tmp = array[i];
+printf("Value checked array[%lu] = [%d]\n", i, tmp);
+if (tmp == value)
+return (i);
+}
+}
+return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,52 @@
+#include "search_algorithms.h"
+
+/**
+ * print_array - prints an array of integers
+ * @array: array to print
+ * @size: size of array
+ *
+ * return: void
+ */
+void print_array(int *array, size_t size)
+{
+size_t i;
+printf("Searching in array:");
+for (i = 0; i < size; i++)
+{
+printf(" %d", array[i]);
+if (i != size - 1)
+printf(",");
+}
+printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: poiter to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: the value to search for
+ * Return: the index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+size_t l, m, r;
+if (array != NULL && size > 0)
+{
+l = 0;
+r = size - 1;
+print_array(array + l, r + 1 - l);
+while (l < r)
+{
+m = (l + r) / 2;
+if (array[m] < value)
+l = m + 1;
+else if (array[m] > value)
+r = m;
+else
+return (m);
+print_array(array + l, r + 1 - l);
+}
+}
+return (-1);
+}
